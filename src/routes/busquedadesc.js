d3095ca37758943fae6963a26abf36945118ef9f
Jesus R <arodriguez@arisoft.com.mx>
2019-09-04 12:48:27 -0600

    build 64

--- src/routes/busquedadesc.js
+++ src/routes/busquedadesc.js
@@ -1,18 +1,18 @@
 const busquedaDesc = require('../models/busquedadesc');
 
-module.exports = function (app){
-
+module.exports = function(app) {
+    //cc
 
     app.get('/busquedaDesc/:busqueda', (req, res) => {
         let busqueda = req.params.busqueda;
-        busquedaDesc.getDescripcion(busqueda,(err, data) => {
-            res.json({busqueda: data});
+        busquedaDesc.getDescripcion(busqueda, (err, data) => {
+            res.json({ busqueda: data });
         });
     });
     app.get('/busquedaDescTodo', (req, res) => {
         let busqueda = req.params.busqueda;
-        busquedaDesc.getDescripcionTodo(busqueda,(err, data) => {
-            res.json({busqueda: data});
+        busquedaDesc.getDescripcionTodo(busqueda, (err, data) => {
+            res.json({ busqueda: data });
         });
     });
 
